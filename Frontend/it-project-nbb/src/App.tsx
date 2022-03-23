80083c8f443426dec05ef20a1d5f39c5462db060
ValerieSleurs <sleurs.valerie@gmail.com>
2022-03-23 21:46:08 +0100

    eerste test Mockoon (NBB API)

--- Frontend/it-project-nbb/src/App.tsx
+++ Frontend/it-project-nbb/src/App.tsx
@@ -1,15 +1,13 @@
 import React, { ChangeEventHandler, useEffect, useState } from 'react';
 import './App.css';
-import { Timezone } from './types';
+import { RootObject } from './types';
 
 //voorlopig staat alle code in de App-component (componenten pas opsplitsen als de volledige structuur in orde is)
 
 const App = () => {
   const [vatNumber1, setVatNumber1] = useState<string>('');
   const [vatNumber2, setVatNumber2] = useState<string>('');
-
-  //voor test met WorldTimeApi
-  const [timezone, setTimezone] = useState<Timezone>();
+  const [referenceNumberData, setReferenceNumberData] = useState<RootObject>();
   const [updating, setUpdating] = useState<boolean>(true);
 
   const handleVatNumber1Change: ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -23,42 +21,40 @@ const App = () => {
   const handleOnClick: React.MouseEventHandler<HTMLInputElement> = (event) => {
   }
 
-  //fetch WorldTimeApi: werkende API testen (NBB down)
+  //fetch NBB API via Mockoon
+  //fetch nog niet gekoppeld aan button click
+  //ondernemingsnummer voorlopig hardcoded in url
   useEffect(() => {
-    getTimezone();
+    getReferenceNumber();
   }, []);
 
-  const getTimezone = async () => {
+  const getReferenceNumber = async () => {
     setUpdating(true);
 
-    let response = await fetch('http://worldtimeapi.org/api/ip', {
+    let response = await fetch('http://localhost:3000/legalEntity/0203201340/references', {
       method: 'GET',
     });
     let json = await response.json();
 
-    setTimezone(json as Timezone);
+    setReferenceNumberData(json as RootObject);
+
     setUpdating(false);
   }
 
-  //incl. output voor test met WorldTimeApi (achteraf deleten)
   return (
     <div>
       <input type="string" id="vatNumber1" name="vatNumber1" placeholder="Ondernemingsnummer 1" onChange={handleVatNumber1Change} value={vatNumber1} />
       <input type="string" id="vatNumber2" name="vatNumber2" placeholder="Ondernemingsnummer 2" onChange={handleVatNumber2Change} value={vatNumber2} />
       <input type="button" id="submit" value="Vergelijk" onClick={handleOnClick} />
       <div>
-        <p>
           Ondernemingsnummer 1: {vatNumber1} - Ondernemingsnummer 2: {vatNumber2}
-        </p>
-        <div>
-          {!timezone || updating ? <div>Loading data</div> :
-            <div>
-              <p>Timezone: <span>{timezone.timezone}</span></p>
-              <p>Abbreviation: <span>{timezone.abbreviation}</span></p>
-              <p>DateTime: <span>{timezone.datetime}</span></p>
-              <p>Client IP: <span>{timezone.client_ip}</span></p>
-            </div>}
-        </div>
+      </div>
+      <div>
+        {!referenceNumberData || updating ? <div>Loading data</div> :
+          <div>
+            <p>Naam: <span>{referenceNumberData.EnterpriseName}</span></p>
+            <p>Ondernemingsnummer: <span>{referenceNumberData.EnterpriseNumber}</span></p>
+          </div>}
       </div>
     </div>
   );
