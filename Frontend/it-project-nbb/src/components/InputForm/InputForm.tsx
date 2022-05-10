1466d935f97ce4df812518ca0ca89da0fe087450
ValerieSleurs <sleurs.valerie@gmail.com>
2022-05-10 09:36:41 +0200

    regex

--- Frontend/it-project-nbb/src/components/InputForm/InputForm.tsx
+++ Frontend/it-project-nbb/src/components/InputForm/InputForm.tsx
@@ -15,14 +15,20 @@ const InputForm = ({ handleVatNumber1Change, handleVatNumber2Change, handleOnCli
           id="vatNumber1"
           name="vatNumber1"
           placeholder="Ondernemingsnummer 1"
+          title="Gelieve een 10-cijferig ondernemingsnummer in te voeren."
+          pattern="[0-9]{10}"
           onChange={handleVatNumber1Change}
+          required
         />
         <input
           type="string"
           id="vatNumber2"
           name="vatNumber2"
-          placeholder="Ondernemingsnummer 2"
+          placeholder="Ondernemingsnummer 2"   
+          title="Gelieve een 10-cijferig ondernemingsnummer in te voeren."       
+          pattern="[0-9]{10}"
           onChange={handleVatNumber2Change}
+          required
         />
         <button
           type="submit"
