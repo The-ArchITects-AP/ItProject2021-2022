073ca9c93efce84ab04e42cf93202f3c97e1ae0e
Claes Qin (s119318) <qin.claes@student.ap.be>
2022-06-15 01:00:13 +0200

    adds GetNameTest

---
+++ Backend/ITProjectAPI/ITProjectAPI.Tests/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITProjectAPI.Viewmodels;
+
+namespace ITProjectAPI.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void GetNameTest()
+        {
+            string kbonummer = "0679720570";
+            NameView result = new NameView()
+            {
+                EnterpriseName = "ROGER VDK+",
+                Street = "Bollaardstraat",
+                Number = "17",
+                PostalCode = "8800",
+                City = "Roeselare"
+            };
+            //OkObjectResult expected = ControllerBase.Ok();
+        }
+    }
+}
