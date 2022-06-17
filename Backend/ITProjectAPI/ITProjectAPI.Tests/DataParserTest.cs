a5e5c418840356671a578adb857dbf7f89f598e3
Claes Qin (s119318) <qin.claes@student.ap.be>
2022-06-17 21:23:08 +0200

    adds DataParserTests

---
+++ Backend/ITProjectAPI/ITProjectAPI.Tests/DataParserTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITProjectAPI.Tests
+{
+    [TestClass]
+    public class DataParserTest
+    {
+        [TestMethod]
+        public void GetEigenVermogenTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetSchuldenTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetBedrijfswinst()
+        {
+            Assert.Fail();
+        }
+    }
+}
